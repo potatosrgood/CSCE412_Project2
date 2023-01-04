6e81b79213400ce6699ada62bcc002e0c4bfd035
ibrahim <ibrahim@tamu.edu>
2023-01-04 10:16:02 +0800

    updated request.h

--- request.h
+++ request.h
@@ -1,9 +1,21 @@
-#include <iostream>
+#include <random>
+#include <string>
 using namespace std;
 
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<> dis(0, 255);
+uniform_int_distribution<> dist(0, 50);
+
+string generateRandomIpAddress() {
+  return std::to_string(dis(gen)) + "." + std::to_string(dis(gen)) + "." + std::to_string(dis(gen)) + "." + std::to_string(dis(gen));
+}
+
 struct Request {
-    int time;
-    string in;
-    string out;
+    
+    int time = dist(gen);
+    string in = generateRandomIpAddress();
+    string out = generateRandomIpAddress();
+
 };
 
