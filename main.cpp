ace21a4aceb362cd9eb5359ebea707323687384c
Thionazin <senhehao@senhehao.com>
2023-01-04 11:49:46 +0800

    fixed bugs with compilation | added run time logic

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <queue>
-#include "request.h"
 #include "LoadBalancer.h"
 
 using namespace std;
 
 
-int NUM_WEBSERVERS = 50
-int MAX_REQUEST_LENGTH = 100
+int NUM_WEBSERVERS = 50;
+int MAX_REQUEST_LENGTH = 100;
 
 int main(){
-  LoadBalancer loadb();
+  cout << "Driver" << endl;
+  LoadBalancer loadb(10);
+  for(int i = 0; i < 100; i++) {
+    Request t_req;
+    loadb.addRequest(t_req);
+  }
+  cout << "Created a load balancer with 100 requests and 10 web servers" << endl;
+  cout << "launching" << endl;
   loadb.launch();
+  cout << "run success" << endl;
   return 1;
 }
--- request.h
+++ request.h
@@ -1,21 +1,28 @@
+#pragma once
 #include <random>
 #include <string>
-using namespace std;
 
-random_device rd;
-mt19937 gen(rd());
-uniform_int_distribution<> dis(0, 255);
-uniform_int_distribution<> dist(0, 50);
+using std::string;
 
-string generateRandomIpAddress() {
-  return std::to_string(dis(gen)) + "." + std::to_string(dis(gen)) + "." + std::to_string(dis(gen)) + "." + std::to_string(dis(gen));
-}
-
-struct Request {
-    
-    int time = dist(gen);
-    string in = generateRandomIpAddress();
-    string out = generateRandomIpAddress();
+class Request {
+  public:
+    Request() { time = generateTime(); string in = generateRandomIpAddress(); string out = generateRandomIpAddress(); };
+    int time;
+    string in;
+    string out;
+  private:
+    string generateRandomIpAddress() {
+      std::random_device rd;
+      std::mt19937 gen(rd());
+      std::uniform_int_distribution<> dis(0, 255);
+      return std::to_string(dis(gen)) + "." + std::to_string(dis(gen)) + "." + std::to_string(dis(gen)) + "." + std::to_string(dis(gen));
+    };
+    int generateTime() {
+      std::random_device rd;
+      std::mt19937 gen(rd());
+      std::uniform_int_distribution<> dist(0, 50);
+      return dist(gen);
+    };
 
 };
 
