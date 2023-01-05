80048bf8f7eb1fd3cc0a427c0280dc55048e82f0
ibrahim <ibrahim@tamu.edu>
2023-01-05 08:57:29 +0800

    added comments

--- WebServer.h
+++ WebServer.h
@@ -2,12 +2,16 @@
 #define WEB_SERVER_H
 
 #include "request.h" // Include the header file for the Request class
-
+/**
+ * @class WebServer
+ * @brief A class representing a web server
+ */
 class WebServer {
  public:
-  int timeLeft = -1;
-  Request currentRequest;
+  int timeLeft = -1; //!< The time remaining for the current task (-1 if no task is being processed)
+  Request currentRequest; //!< The request currently being processed
 
+  
   WebServer() {}
   void startNewRequest(Request r);
   bool iterate();
--- request.h
+++ request.h
@@ -5,23 +5,34 @@
 
 using std::string;
 
+/**
+ * @class Request
+ * @brief A class representing a request to a web server
+ */
 class Request {
   public:
+    /**
+     * @brief Constructs a new request with random time, in and out values
+     */
     Request() { time = generateTime(); in = generateRandomIpAddress(); out = generateRandomIpAddress(); };
-    int time;
-    string in;
-    string out;
+    int time;  //!< The time required to process the request
+    string in; //!< The IP address of the client making the request
+    string out; //!< The IP address of the server to which the request is being sent
   private:
-    // generates a random ip address string
-    // created by ChatGPT
+    /**
+     * @brief Generates a random IP address
+     * @return A string representing the generated IP address
+     */
     string generateRandomIpAddress() {
       std::random_device rd;
       std::mt19937 gen(rd());
       std::uniform_int_distribution<> dis(0, 255);
       return std::to_string(dis(gen)) + "." + std::to_string(dis(gen)) + "." + std::to_string(dis(gen)) + "." + std::to_string(dis(gen));
     };
-    // generates a random time
-    // created by ChatGPT
+    /**
+     * @brief Generates a random "time" value in the range [2, 50)
+     * @return The generated time value
+     */
     int generateTime() {
       std::random_device rd;
       std::mt19937 gen(rd());
