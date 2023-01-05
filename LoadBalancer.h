56d8d054623e370f983f91503301652fe9d43464
ibrahim <ibrahim@tamu.edu>
2023-01-05 09:44:45 +0800

    more comments

--- LoadBalancer.h
+++ LoadBalancer.h
@@ -11,9 +11,26 @@ using std::vector;
 using std::cout;
 using std::endl;
 
+/**
+ * @class LoadBalancer
+ * @brief A class representing a load balancer that distributes requests to a pool of web servers
+ */
 class LoadBalancer {
   public:
+    /**
+     * @brief Constructs a new load balancer with the given number of servers
+     * @param serverCount The number of servers to create
+     */
     LoadBalancer(int serverCount);
+
+    /**
+     * @brief Performs a single clock cycle for the load balancer
+     *
+     * This function iterates through all the servers and checks if any of them
+     * have completed their current task. If a server is available, it will start
+     * processing the next request in the queue. If no servers are available and
+     * the request queue is empty, it sets the `serversDone` flag to true.
+     */
     void performCycle();
     bool hasItems() { return !serversDone; };
     int getTime() { return clockTime; };
